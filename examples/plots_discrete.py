2edc63cd48aa4e2671e266d17b14ad014b5a02d1
garcia-mc <carmeiga97@gmail.com>
2022-04-02 09:28:33 +0200

    discrete graphs

--- examples/plots_discrete.py
+++ examples/plots_discrete.py
@@ -14,12 +14,12 @@ q=np.multiply(np.asmatrix(Jinford[6,Jinford[7,:]==1]).transpose(),np.exp(model.m
 q0=np.multiply(np.asmatrix(Jinford[6,Jinford[7,:]==0]).transpose(),np.exp(model.model.forward(Xt0[Jinford[1,Jinford[7,:]==0].astype(int),:]).detach().numpy()))
 # NEVER FORGET WE ARE ESTIMATING H_ZERO!!!!!!
 
-plt.plot(Jinford[0,Jinford[7,:]==0],np.exp(-np.asarray(q0)),c='green',drawstyle='steps-post', label='steps-post',linewidth=0.5)
-plt.plot(Jinford[0,Jinford[7,:]==1],np.exp(-np.asarray(q)),c='orange',drawstyle='steps-post', label='steps-post',linewidth=0.5)
+plt.plot(Jinford[0,Jinford[7,:]==0],np.exp(-np.asarray(q0)),c='green',drawstyle='steps-post', label='steps-post',linewidth=0.75)
+plt.plot(Jinford[0,Jinford[7,:]==1],np.exp(-np.asarray(q)),c='red',drawstyle='steps-post', label='steps-post',linewidth=0.75)
 
 
 
 plt.xlabel('Time', fontsize=12)
-plt.ylabel('Baseline survival function', fontsize=12)
+plt.ylabel('Survival functions', fontsize=12)
 
 plt.savefig('../images/nonlinear_surv.png', dpi=300)
